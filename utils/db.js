ed89e1742ec9de0693b1c5d70c0999db92241a69
Grace O. Nweke <lagra2001@gmail.com>
2024-05-17 18:33:47 -0700

    fixing task 0

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,39 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-    constructor() {
-        const {
-            DB_HOST = 'localhost',
-            DB_PORT = 27017,
-            DB_DATABASE = 'files_manager'
-        } = process.env;
+  constructor() {
+    const {
+      DB_HOST = 'localhost',
+      DB_PORT = 27017,
+      DB_DATABASE = 'files_manager',
+    } = process.env;
 
-        // MongoDB connection URI
-        this.URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+    // MongoDB connection URI
+    this.URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
-        // Initialize MongoDB client
-        this.client = new MongoClient(this.URI, { useUnifiedTopology: true });
-        this.client.connect();
+    // Initialize MongoDB client
+    this.client = new MongoClient(this.URI, { useUnifiedTopology: true });
+    this.client.connect();
 
-        // MongoDB collections
-        this.usersCollection = this.client.db().collection('users');
-        this.filesCollection = this.client.db().collection('files');
-    }
+    // MongoDB collections
+    this.usersCollection = this.client.db().collection('users');
+    this.filesCollection = this.client.db().collection('files');
+  }
 
-    isAlive() {
-        // Check if the MongoDB client is connected
-        return this.client.isConnected();
-    }
+  isAlive() {
+    // Check if the MongoDB client is connected
+    return this.client.isConnected();
+  }
 
-    async nbUsers() {
-        // Get the number of documents in the users collection
-        return this.usersCollection.countDocuments();
-    }
+  async nbUsers() {
+    // Get the number of documents in the users collection
+    return this.usersCollection.countDocuments();
+  }
 
-    async nbFiles() {
-        // Get the number of documents in the files collection
-        return this.filesCollection.countDocuments();
-    }
+  async nbFiles() {
+    // Get the number of documents in the files collection
+    return this.filesCollection.countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
--- utils/redis.js
+++ utils/redis.js
@@ -1,58 +1,58 @@
 import redis from 'redis';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
+  constructor() {
+    this.client = redis.createClient();
 
-        // Display any errors from the redis client
-        this.client.on('error', (err) => {
-            console.error('Redis Client Error:', err);
-        });
-    }
+    // Display any errors from the redis client
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error:', err);
+    });
+  }
 
-    isAlive() {
-        // Check if the redis client is connected
-        return this.client.connected;
-    }
+  isAlive() {
+    // Check if the redis client is connected
+    return this.client.connected;
+  }
 
-    async get(key) {
-        // Get the value for the specified key from Redis
-        return new Promise((resolve, reject) => {
-            this.client.get(key, (err, value) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(value);
-                }
-            });
-        });
-    }
+  async get(key) {
+    // Get the value for the specified key from Redis
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
 
-    async set(key, value, durationInSeconds) {
-        // Set the value for the specified key in Redis with an expiration
-        return new Promise((resolve, reject) => {
-            this.client.setex(key, durationInSeconds, value, (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-    }
+  async set(key, value, durationInSeconds) {
+    // Set the value for the specified key in Redis with an expiration
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, durationInSeconds, value, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
 
-    async del(key) {
-        // Delete the value for the specified key from Redis
-        return new Promise((resolve, reject) => {
-            this.client.del(key, (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-    }
+  async del(key) {
+    // Delete the value for the specified key from Redis
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
 }
 
 const redisClient = new RedisClient();
